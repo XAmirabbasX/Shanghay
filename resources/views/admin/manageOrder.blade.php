a9d181a2667782f414b24290d09c5e7279c65664
XAmirabbasX <148373175+XAmirabbasX@users.noreply.github.com>
2025-09-24 18:36:19 +0330

    add priority to project with database

--- resources/views/admin/manageOrder.blade.php
+++ resources/views/admin/manageOrder.blade.php
@@ -83,13 +83,13 @@
                                             </td>
                                             <td>
                                                 <a href="{{route('admin.deleteOrder', $order->id)}}" class="btn btn-icon btn-light btn-hover-primary btn-sm">
-                                                    <span class="svg-icon svg-icon-md svg-icon-primary">
-                                                        <!--begin::Svg Icon | path:assets/media/svg/icons/General/Trash.svg-->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                                <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"></path>
-                                                            </g>
-                                                        </svg>
-                                                <!--end::Svg Icon-->
+                                                    <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\legacy\metronic\theme\html\demo1\dist/../src/media/svg/icons\General\Trash.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                            <rect x="0" y="0" width="24" height="24"/>
+                                                            <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero"/>
+                                                            <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"/>
+                                                        </g>
+                                                        </svg><!--end::Svg Icon-->
                                                     </span>
                                                 </a>
                                                 <form class = "btn btn-icon btn-light btn-hover-primary btn-sm mx-3" action="{{route('admin.detailsOrder', $order->id)}}">
@@ -107,9 +107,24 @@
                                                                 <rect fill="#000000" opacity="0.3" x="7" y="17" width="2" height="2" rx="1"/>
                                                                 <rect fill="#000000" opacity="0.3" x="10" y="17" width="7" height="2" rx="1"/>
                                                             </g>
+                                                            </svg>
+                                                        </span>
+                                                    </button>
+                                                </form>
+                                                <form class = "btn btn-icon btn-light btn-hover-primary btn-sm mx-3" action="{{route('admin.detailsOrder', $order->id)}}">
+                                                    @csrf
+                                                    <button class = "btn btn-icon btn-light btn-hover-primary btn-sm mx-3">
+                                                        <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\legacy\metronic\theme\html\demo1\dist/../src/media/svg/icons\General\Fire.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                                <rect x="0" y="0" width="24" height="24"/>
+                                                                <path d="M14,7 C13.6666667,10.3333333 12.6666667,12.1167764 11,12.3503292 C11,12.3503292 12.5,6.5 10.5,3.5 C10.5,3.5 10.287918,6.71444735 8.14498739,10.5717225 C7.14049032,12.3798172 6,13.5986793 6,16 C6,19.428689 9.51143904,21.2006583 12.0057195,21.2006583 C14.5,21.2006583 18,20.0006172 18,15.8004732 C18,14.0733981 16.6666667,11.1399071 14,7 Z" fill="#000000"/>
+                                                            </g>
+                                                            </svg><!--end::Svg Icon-->
+                                                        </span>
                                                     </button>
                                                 </form>
                                             </td>
+                                        </tr>
                                 </tbody>
                                 @endforeach
                                 @endif
